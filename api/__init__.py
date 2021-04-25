663bcd3887fae9aaf04a7aac1a27e5969d79387d
BeaTea109 <beatea109@hotmail.com>
2021-04-25 01:39:57 -0500

    updated

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, config
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
+from .config import Config
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://xsdieiszkzggiy:3642b3efac62c3395d4d7ad9134895617db0fd25e205e557cb2f1fa011932c2c@ec2-34-225-103-117.compute-1.amazonaws.com:5432/dfesuqo3805m8u"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
+
 
 db = SQLAlchemy(app)
 CORS(app)
@@ -14,5 +13,5 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-app.config.from_object(__name__)
+app.config.from_object(Config)
 from api.views import views
